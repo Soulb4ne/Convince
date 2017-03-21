3d1cf90cc7468a9cc6ebb61e1f3bc5557e566556
Vince Pauli <Soulb4ne@gmail.com>
2017-03-21 23:47:46 +0100

    Spigot Template
    Empty and unworked Spigot Plugin

---
+++ SpigotPluginTemplate/src/com/convinced/plugin/SpigotPluginTemplate.java
@@ -0,0 +1,13 @@
+package com.convinced.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotPluginTemplate extends JavaPlugin {
+
+	public void onEnable() {
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
